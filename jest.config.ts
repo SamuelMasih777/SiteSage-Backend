6f2b0cec17bbbfcf2b48d0de87889459e4a0e725
Samuel Masih <samuelmasih.sls777@gmail.com>
2025-12-19 05:23:13 +0530

    chore: Simplify Jest configuration by removing ESM-specific settings and adding verbose and force exit options.

--- jest.config.ts
+++ jest.config.ts
@@ -1,24 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
-
-  extensionsToTreatAsEsm: ['.ts'],
-
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: true }],
-  },
-
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-
-  transformIgnorePatterns: [
-    'node_modules/(?!uuid)',
-  ],
-
+  verbose: true,
+  forceExit: true,
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!uuid)',
+  ],
 };
